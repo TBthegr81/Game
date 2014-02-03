03d142d8d20ebb029d4b819690c2a63019ebddb9
TBthegr81 <tbthegr81@gmail.com>
2014-02-03 14:41:34 +0100

    TB-paket
    Full of TB

--- Kungsholmen/src/TROLOLOLOl.java
+++
@@ -1,6 +0,0 @@
-
-public class TROLOLOLOl {
-//troll
-	
-	//lolol
-}
---
+++ Kungsholmen/src/com/game/TB/MainMenuScreen.java
@@ -0,0 +1,80 @@
+package com.game.TB;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
+
+    final GBS game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final GBS gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+        //...Rest of class omitted for succinctness.
+
+}
